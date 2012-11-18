7e28ab246583198c4800bdf005f26a1ac6514773
Gareth Rushgrove <gareth@morethanseven.net>
2012-11-18 14:24:18 +0000

    use the automatic fixtures feature from the puppetlabs spec helper

--- Gemfile
+++ Gemfile
@@ -10,3 +10,4 @@ gem "rake"
 gem "puppet", puppetversion
 gem "puppet-lint"
 gem "rspec-puppet", "0.1.4"
+gem "puppetlabs_spec_helper"
--- spec/classes/freight/freight_spec.rb
+++ spec/classes/freight/freight_spec.rb
@@ -1,4 +1,4 @@
-require_relative 'spec_helper'
+require 'spec_helper'
 
 describe 'freight', :type => :class do
   let(:facts) { {:lsbdistcodename => "lucid"} }
