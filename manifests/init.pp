d15077cc3d590af73744d4d91d7ee65d1b1389f2
Gareth Rushgrove <gareth@morethanseven.net>
2013-10-04 17:27:54 +0100

    fix quoting of non-variable strings

--- manifests/init.pp
+++ manifests/init.pp
@@ -6,8 +6,8 @@ class freight {
   apt::source { 'rcrowley':
     location    => 'http://packages.rcrowley.org',
     repos       => 'main',
-    key         => "7DF49CEF",
-    key_source  => "http://packages.rcrowley.org/keyring.gpg",
+    key         => '7DF49CEF',
+    key_source  => 'http://packages.rcrowley.org/keyring.gpg',
     include_src => false,
   }
 }
