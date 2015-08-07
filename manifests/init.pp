aafe09758c54c1b3bb32c0c4c2cfcd3b0389061e
Davide Ferrari <davide@trovit.com>
2015-08-07 12:54:38 +0200

    Configure Freight with dynamic parameters

--- manifests/init.pp
+++ manifests/init.pp
@@ -1,4 +1,12 @@
-class freight {
+class freight ( $lib_dir         = '/var/lib/freight',
+                $cache_dir       = '/var/cache/freight',
+                $origin          = 'Freight',
+                $label           = 'Freight',
+                $enable_cache    = false,
+                $gpg_key_address,
+                $enable_symlinks = false,
+              ) {
+
   package { 'freight':
     require => Apt::Source['rcrowley'],
   }
@@ -10,4 +18,27 @@ class freight {
     key_source  => 'http://packages.rcrowley.org/keyring.gpg',
     include_src => false,
   }
+
+  file { $lib_dir:
+    ensure => directory,
+    owner  => 'root',
+    group  => 'root',
+    mode   => '0755',
+  }
+
+  file { $cache_dir:
+    ensure => directory,
+    owner  => 'root',
+    group  => 'root',
+    mode   => '0755',
+  }
+
+  file { '/etc/freight.conf':
+    ensure  => file,
+    owner   => 'root',
+    group   => 'root',
+    mode    => '0644',
+    content => template('freight/freight.conf.erb'),
+  }
+  
 }
